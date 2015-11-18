ac800b46fc9740ef167698348edc0f43c00397f8
Wei Song <wsong83@gmail.com>
2015-11-18 15:04:37 +0000

    fix a warning in verilator

--- util/arbiter.sv
+++ util/arbiter.sv
@@ -15,16 +15,31 @@ module arbiter_rr
    logic [2*N-1:0] mask, req_ext, p_ext;
    logic           lock, locked; // make the arbiter stable
 
+`ifdef VERILATOR
+   logic [2*N-1:0] p_or, req_and;
+`endif
+
    genvar          i;
 
    assign mask[0] = 1'b0;
    assign req_ext = {req, req};
    assign p_ext = p;
 
+`ifdef VERILATOR
+   generate
+      assign p_or[0] = 0;
+      assign req_and = req_ext & p_or;
+      for(i=1; i<2*N; i++) begin
+         assign p_or[i] = |p_ext[i-1:0];
+         assign mask[i] = ~|req_and[i-1:0] & req_and[i];
+      end
+   endgenerate
+`else
    generate
       for(i=1; i<2*N; i++)
         assign mask[i] = (mask[i-1] && !req_ext[i-1]) || p_ext[i-1];
    endgenerate
+`endif
 
    assign gnt = enable ? (locked ? p : req & (mask[N-1:0] | mask[2*N-1:N])) : 0;
 
